2d63f96421dee864540d8dc8850f5e2cfce24a43
Gohér László <goher.laci@gmail.com>
2023-08-05 10:13:59 +0200

    greatly simplified eval function

--- src/native/eval/mod.rs
+++ src/native/eval/mod.rs
@@ -3,7 +3,7 @@ use crate::util::*;
 use crate::native::print::print;
 
 
-struct Atom {
+struct AtomFrame {
     value: GcRef,
     environment: GcRef,
     in_call: bool,
@@ -17,7 +17,7 @@ enum ConsProgress {
 }
 
 
-struct Cons {
+struct ConsFrame {
     car: GcRef,
     cdr: GcRef,
     progress: ConsProgress,
@@ -34,7 +34,7 @@ enum ListKind {
 }
 
 
-struct List {
+struct ListFrame {
     kind: ListKind,
     elems: Vec<GcRef>,
     current: usize,
@@ -44,9 +44,9 @@ struct List {
 
 
 enum StackFrame {
-    Atom(Atom),
-    Cons(Cons),
-    List(List),
+    Atom(AtomFrame),
+    Cons(ConsFrame),
+    List(ListFrame),
 }
 
 impl StackFrame {
@@ -68,13 +68,13 @@ impl StackFrame {
             else {
                 ListKind::Empty
             };
-            Self::List(List{ kind, elems: vec, current: 0, environment, in_call: false })
+            Self::List(ListFrame{ kind, elems: vec, current: 0, environment, in_call: false })
         }
         else if let PrimitiveValue::Cons(cons) = x.get() {
-            Self::Cons(Cons{ car: cons.get_car(), cdr: cons.get_cdr(), progress: ConsProgress::NotStartedYet, environment })
+            Self::Cons(ConsFrame{ car: cons.get_car(), cdr: cons.get_cdr(), progress: ConsProgress::NotStartedYet, environment })
         }
         else {
-            Self::Atom(Atom{ value: x, environment, in_call: false })
+            Self::Atom(AtomFrame{ value: x, environment, in_call: false })
         }
     }
 }
@@ -83,6 +83,7 @@ impl StackFrame {
 enum EvalInternal {
     Return(GcRef),
     Call(GcRef, GcRef),
+    TailCall(GcRef, GcRef),
     Signal(GcRef),
     Abort(String),
 }
@@ -106,10 +107,16 @@ fn lookup(mem: &Memory, key: GcRef, environment: GcRef) -> Option<GcRef> {
 }
 
 
-fn eval_atom(mem: &mut Memory, atom: GcRef, environment: GcRef) -> EvalInternal {
+fn eval_atom(mem: &mut Memory, atom_frame: &mut AtomFrame, return_value: GcRef) -> EvalInternal {
+    if atom_frame.in_call {
+        return EvalInternal::Return(return_value);
+    }
+    
+    let atom = atom_frame.value.clone();
+    let env  = atom_frame.environment.clone();
     match atom.get() {
         PrimitiveValue::Symbol(_) => {
-            if let Some(value) = lookup(mem, atom, environment) {
+            if let Some(value) = lookup(mem, atom, env) {
                 EvalInternal::Return(value)
             }
             else {
@@ -120,10 +127,82 @@ fn eval_atom(mem: &mut Memory, atom: GcRef, environment: GcRef) -> EvalInternal 
             unreachable!("eval_atom received a conscell, but conscells shuld be processed in eval_internal")
         },
         PrimitiveValue::Trap(trap) => {
-            EvalInternal::Call(trap.get_normal_body(), environment)
+            atom_frame.in_call = true;
+            EvalInternal::Call(trap.get_normal_body(), env)
         },
         _ => {
             EvalInternal::Return(atom)
+        },
+    }
+}
+
+fn eval_list(mem: &mut Memory, list_frame: &mut ListFrame, return_value: GcRef) -> EvalInternal {
+    match list_frame.kind {
+        ListKind::Empty => {
+            EvalInternal::Return(GcRef::nil())
+        },
+        ListKind::BadOperator => {
+            EvalInternal::Signal(mem.symbol_for("bad-operator"))
+        },
+        ListKind::Lambda | ListKind::SpecialLambda => {
+            if list_frame.in_call {
+                list_frame.elems[list_frame.current] = return_value.clone();
+                list_frame.current += 1;
+                list_frame.in_call = false;
+            }
+
+            let i = list_frame.current;
+            let top =
+            if list_frame.kind == ListKind::Lambda {
+                // if lambda then evaluate the operator and the operands
+                list_frame.elems.len()
+            }
+            else {
+                // if special-lambda then only evaluate the operator
+                1
+            };
+
+            if i < top {
+                let x = list_frame.elems[i].clone();
+                list_frame.in_call = true;
+                let env = list_frame.environment.clone();
+                return EvalInternal::Call(x, env);
+            }
+
+            let mut new_env = list_frame.environment.clone();
+            let operator    = list_frame.elems[0].get().as_function();
+            for (i, param) in operator.params().enumerate() {
+                let arg       = list_frame.elems[i + 1].clone();
+                let param_arg = mem.allocate_cons(param, arg);
+                new_env       = mem.allocate_cons(param_arg, new_env);
+            }
+
+            let new_tree = operator.get_body();
+            EvalInternal::TailCall(new_tree, new_env)
+        },
+    }
+}
+
+
+fn eval_cons(mem: &mut Memory, cons_frame: &mut ConsFrame, return_value: GcRef) -> EvalInternal {
+    match cons_frame.progress {
+        ConsProgress::NotStartedYet => {
+            let car = cons_frame.car.clone();
+            let env = cons_frame.environment.clone();
+            cons_frame.progress = ConsProgress::EvalingCar;
+            EvalInternal::Call(car, env)
+        },
+        ConsProgress::EvalingCar => {
+            cons_frame.car = return_value.clone();
+
+            let cdr = cons_frame.cdr.clone();
+            let env = cons_frame.environment.clone();
+            cons_frame.progress = ConsProgress::EvalingCdr;
+            EvalInternal::Call(cdr, env)
+        },
+        ConsProgress::EvalingCdr => {
+            cons_frame.cdr = return_value.clone();
+            EvalInternal::Return(mem.allocate_cons(cons_frame.car.clone(), cons_frame.cdr.clone()))
         },
     }
 }
@@ -147,141 +226,61 @@ fn unwind_stack(mem: &mut Memory, stack: &mut Vec<StackFrame>, signal: GcRef) ->
 }
 
 
-fn eval_internal(mem: &mut Memory, tree: GcRef, environment: GcRef) -> EvalInternal {
+fn eval_internal(mem: &mut Memory, tree: GcRef, environment: GcRef) -> Result<GcRef, String> {
     let mut stack        = vec![StackFrame::new(tree, environment)];
     let mut return_value = GcRef::nil();
 
-    'stack_loop: while let Some(frame) = stack.last_mut() {
+    while let Some(frame) = stack.last_mut() {
+        let evaled =
         match frame {
-            StackFrame::Atom(atom_frame) => {
-                if atom_frame.in_call {
-                    // coming back from the normal body of a trap, nothing to do here
+            StackFrame::Atom(ref mut atom_frame) => {
+                eval_atom(mem, atom_frame, return_value.clone())
+            },
+            StackFrame::Cons(ref mut cons_frame) => {
+                eval_cons(mem, cons_frame, return_value.clone())
+            },
+            StackFrame::List(ref mut list_frame) => {
+                eval_list(mem, list_frame, return_value.clone())
+            },
+        };
+
+        match evaled {
+            EvalInternal::Return(x) => {
+                return_value = x;
+            },
+            EvalInternal::Call(new_tree, new_env) => {
+                stack.push(StackFrame::new(new_tree, new_env));
+                continue;
+            },
+            EvalInternal::TailCall(new_tree, new_env) => {
+                *frame = StackFrame::new(new_tree, new_env);
+                continue;
+            },
+            EvalInternal::Signal(signal) => {
+                if let Some(trap_frame) = unwind_stack(mem, &mut stack, signal.clone()) {
+                    stack.push(trap_frame);
+                    continue;
                 }
                 else {
-                    match eval_atom(mem, atom_frame.value.clone(), atom_frame.environment.clone()) {
-                        EvalInternal::Return(x) => {
-                            return_value = x;
-                        },
-                        EvalInternal::Call(new_tree, new_env) => {
-                            atom_frame.in_call = true;
-                            stack.push(StackFrame::new(new_tree, new_env));
-                            continue 'stack_loop;
-                        },
-                        EvalInternal::Signal(signal) => {
-                            if let Some(trap_frame) = unwind_stack(mem, &mut stack, signal.clone()) {
-                                stack.push(trap_frame);
-                                continue 'stack_loop;
-                            }
-                            else {
-                                return EvalInternal::Signal(signal);
-                            }
-                        },
-                        EvalInternal::Abort(msg) => {
-                            return EvalInternal::Abort(msg);
-                        },
-                    }
+                    let abort_msg = format!("Unhandled signal: {}", list_to_string(print(mem, signal)).unwrap());
+                    return Err(abort_msg);
                 }
             },
-            StackFrame::Cons(cons_frame) => {
-                match cons_frame.progress {
-                    ConsProgress::NotStartedYet => {
-                        let car = cons_frame.car.clone();
-                        let env = cons_frame.environment.clone();
-                        cons_frame.progress = ConsProgress::EvalingCar;
-                        stack.push(StackFrame::new(car, env));
-                        continue 'stack_loop;
-                    },
-                    ConsProgress::EvalingCar => {
-                        cons_frame.car = return_value.clone();
-
-                        let cdr = cons_frame.cdr.clone();
-                        let env = cons_frame.environment.clone();
-                        cons_frame.progress = ConsProgress::EvalingCdr;
-                        stack.push(StackFrame::new(cdr, env));
-
-                        continue 'stack_loop;
-                    },
-                    ConsProgress::EvalingCdr => {
-                        cons_frame.cdr = return_value.clone();
-                        return_value = mem.allocate_cons(cons_frame.car.clone(), cons_frame.cdr.clone());
-                    },
-                }
-            },
-            StackFrame::List(list_frame) => {
-                match list_frame.kind {
-                    ListKind::Empty => {
-                        return_value = GcRef::nil();
-                    },
-                    ListKind::BadOperator => {
-                        let signal = mem.symbol_for("bad-operator");
-
-                        if let Some(trap_frame) = unwind_stack(mem, &mut stack, signal.clone()) {
-                            stack.push(trap_frame);
-                            continue 'stack_loop;
-                        }
-                        else {
-                            return EvalInternal::Signal(signal);
-                        }
-                    },
-                    ListKind::Lambda | ListKind::SpecialLambda => {
-                        if list_frame.in_call {
-                            list_frame.elems[list_frame.current] = return_value.clone();
-                            list_frame.current += 1;
-                            list_frame.in_call = false;
-                        }
-
-                        let i = list_frame.current;
-                        let top =
-                        if list_frame.kind == ListKind::Lambda {
-                            // if lambda then evaluate the operator and the operands
-                            list_frame.elems.len()
-                        }
-                        else {
-                            // if special-lambda then only evaluate the operator
-                            1
-                        };
-
-                        if i < top {
-                            let x = list_frame.elems[i].clone();
-                            list_frame.in_call = true;
-                            let env = list_frame.environment.clone();
-                            stack.push(StackFrame::new(x, env));
-                            continue 'stack_loop;
-                        }
-
-                        let mut new_env = list_frame.environment.clone();
-                        let operator    = list_frame.elems[0].get().as_function();
-                        for (i, param) in operator.params().enumerate() {
-                            let arg       = list_frame.elems[i + 1].clone();
-                            let param_arg = mem.allocate_cons(param, arg);
-                            new_env       = mem.allocate_cons(param_arg, new_env);
-                        }
-
-                        let new_tree = operator.get_body();
-                        *frame = StackFrame::new(new_tree, new_env);
-
-                        continue 'stack_loop;
-                    },
-                }
+            EvalInternal::Abort(msg) => {
+                return Err(msg);
             },
         }
 
         stack.pop();
     }
 
-    EvalInternal::Return(return_value)
+    Ok(return_value)
 }
 
 
 pub fn eval(mem: &mut Memory, tree: GcRef) -> Result<GcRef, String> {
     let empty_env = GcRef::nil();
-    
-    match eval_internal(mem, tree, empty_env) {
-        EvalInternal::Return(x)      => Ok(x),
-        EvalInternal::Call(_, _)     => unreachable!("`eval_internal` returned `EvalInternal::Call` when called from `eval`"),
-        EvalInternal::Signal(signal) => Err(format!("Unhandled signal: {}", list_to_string(print(mem, signal)).unwrap())),
-        EvalInternal::Abort(msg)     => Err(msg),
-    }
+    eval_internal(mem, tree, empty_env)
 }
 
 
