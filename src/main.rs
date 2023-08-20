5050edbee8d05217a2fd67548788126605ec7512
Gohér László <goher.laci@gmail.com>
2023-08-20 17:37:52 +0200

    loading of native functions to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,14 @@
 use crate::memory::*;
 use crate::util::vec_to_list;
 use crate::native::eval::eval_external;
+use crate::native::load_native_functions;
 
 fn main() {
     println!("Hello, world!");
 
     let mut mem = Memory::new();
 
-    let empty_env = GcRef::nil();
-
-    let repl = mem.allocate_native_function(FunctionKind::Lambda, crate::native::repl::repl, empty_env.clone());
-    mem.define_global("repl", repl);
-
-    let lambda = mem.allocate_native_function(FunctionKind::SpecialLambda, crate::native::functions::lambda, empty_env.clone());
-    mem.define_global("lambda", lambda);
-
-    let cons = mem.allocate_native_function(FunctionKind::Lambda, crate::native::list::cons, empty_env.clone());
-    mem.define_global("cons", cons);
-
-    let car = mem.allocate_native_function(FunctionKind::Lambda, crate::native::list::car, empty_env.clone());
-    mem.define_global("car", car);
-
-    let cdr = mem.allocate_native_function(FunctionKind::Lambda, crate::native::list::cdr, empty_env.clone());
-    mem.define_global("cdr", cdr);
-
-    let list = mem.allocate_native_function(FunctionKind::Lambda, crate::native::list::list, empty_env.clone());
-    mem.define_global("list", list);
-
-    let quote = mem.allocate_native_function(FunctionKind::SpecialLambda, crate::native::misc::quote, empty_env.clone());
-    mem.define_global("quote", quote);
-
-    let branch = mem.allocate_native_function(FunctionKind::SpecialLambda, crate::native::misc::branch, empty_env.clone());
-    mem.define_global("if", branch);
-
-    let equal = mem.allocate_native_function(FunctionKind::Lambda, crate::native::misc::equal, empty_env.clone());
-    mem.define_global("=", equal);
-
-    let abort = mem.allocate_native_function(FunctionKind::Lambda, crate::native::misc::abort, empty_env.clone());
-    mem.define_global("abort", abort);
-
-    let signal = mem.allocate_native_function(FunctionKind::Lambda, crate::native::signal::signal, empty_env.clone());
-    mem.define_global("signal", signal);
-
-    let trap = mem.allocate_native_function(FunctionKind::SpecialLambda, crate::native::signal::trap, empty_env.clone());
-    mem.define_global("trap", trap);
-
-    let eval = mem.allocate_native_function(FunctionKind::Lambda, crate::native::eval::eval, empty_env.clone());
-    mem.define_global("eval", eval);
-
-    let add = mem.allocate_native_function(FunctionKind::Lambda, crate::native::numbers::add, empty_env.clone());
-    mem.define_global("add", add);
-
-    let multiply = mem.allocate_native_function(FunctionKind::Lambda, crate::native::numbers::multiply, empty_env.clone());
-    mem.define_global("multiply", multiply);
-
-    let divide = mem.allocate_native_function(FunctionKind::Lambda, crate::native::numbers::divide, empty_env.clone());
-    mem.define_global("divide", divide);
+    load_native_functions(&mut mem);
 
     let vec = vec![mem.symbol_for("repl")];
     let expression = vec_to_list(&mut mem, vec);
--- src/native/mod.rs
+++ src/native/mod.rs
@@ -1,5 +1,38 @@
 #![allow(dead_code)]
 
+use crate::memory::*;
+
+
+
+pub fn load_native_functions(mem: &mut Memory) {
+    load_native_function(mem, "lambda",   functions::lambda, FunctionKind::SpecialLambda);
+    load_native_function(mem, "cons",     list::cons,        FunctionKind::Lambda);
+    load_native_function(mem, "car",      list::car,         FunctionKind::Lambda);
+    load_native_function(mem, "cdr",      list::cdr,         FunctionKind::Lambda);
+    load_native_function(mem, "list",     list::list,        FunctionKind::Lambda);
+    load_native_function(mem, "quote",    misc::quote,       FunctionKind::SpecialLambda);
+    load_native_function(mem, "if",       misc::branch,      FunctionKind::SpecialLambda);
+    load_native_function(mem, "=",        misc::equal,       FunctionKind::Lambda);
+    load_native_function(mem, "abort",    misc::abort,       FunctionKind::Lambda);
+    load_native_function(mem, "signal",   signal::signal,    FunctionKind::Lambda);
+    load_native_function(mem, "trap",     signal::trap,      FunctionKind::SpecialLambda);
+    load_native_function(mem, "read",     read::read,        FunctionKind::Lambda);
+    load_native_function(mem, "eval",     eval::eval,        FunctionKind::Lambda);
+    load_native_function(mem, "print",    print::print,      FunctionKind::Lambda);
+    load_native_function(mem, "add",      numbers::add,      FunctionKind::Lambda);
+    load_native_function(mem, "multiply", numbers::multiply, FunctionKind::Lambda);
+    load_native_function(mem, "divide",   numbers::divide,   FunctionKind::Lambda);
+    load_native_function(mem, "repl",     repl::repl,        FunctionKind::Lambda);
+}
+
+fn load_native_function(mem: &mut Memory, name: &str, function: fn(&mut Memory, &[GcRef], GcRef) -> NativeResult, kind: FunctionKind) {
+    let empty_env = GcRef::nil();
+
+    let nf = mem.allocate_native_function(kind, function, empty_env);
+    mem.define_global(name, nf);
+}
+
+
 
 pub mod print;
 pub mod read;
