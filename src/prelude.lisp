27e20e9988c8c83f7fd662ccc8e29bfed657620c
Gohér László <goher.laci@gmail.com>
2023-08-25 18:54:24 +0200

    macros in prelude: defmacro, defun

--- src/prelude.lisp
+++ src/prelude.lisp
@@ -1,3 +1,9 @@
 (define t (quote t))
 
 (define nil ())
+
+(define defmacro (macro (name params body)
+    (list (quote define) name (list (quote macro) params body))))
+
+(defmacro defun (name params body)
+  (list (quote define) name (list (quote lambda) params body)))
