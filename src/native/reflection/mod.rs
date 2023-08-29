70cd2d3fbd16c9a2d3283d768301ca37c4a79c83
Gohér László <goher.laci@gmail.com>
2023-08-29 11:35:51 +0200

    native-function: get-metadata

--- src/native/reflection/mod.rs
+++ src/native/reflection/mod.rs
@@ -1,9 +1,9 @@
 use crate::memory::*;
+use crate::util::*;
 
 pub fn type_of(mem: &mut Memory, args: &[GcRef], _env: GcRef) -> NativeResult {
     if args.len() != 1 {
-        let signal = mem.symbol_for("wrong-number-of-arguments");
-        return NativeResult::Signal(signal);
+        return NativeResult::Signal(mem.symbol_for("wrong-number-of-arguments"));
     }
 
     if let Some(x) = args[0].get() {
@@ -31,4 +31,30 @@ pub fn type_of(mem: &mut Memory, args: &[GcRef], _env: GcRef) -> NativeResult {
     else {
         NativeResult::Value(mem.symbol_for("nil"))
     }
+}
+
+pub fn get_metadata(mem: &mut Memory, args: &[GcRef], _env: GcRef) -> NativeResult {
+    if args.len() != 1 {
+        return NativeResult::Signal(mem.symbol_for("wrong-number-of-arguments"));
+    }
+
+    let metadata;
+    if let Some(x) = args[0].get_metadata() {
+        metadata = x;
+    }
+    else {
+        return NativeResult::Value(GcRef::nil());
+    }
+
+    let doc_sym    = mem.symbol_for("documentation");
+    let doc        = string_to_list(mem, &metadata.documentation);
+    let file_sym   = mem.symbol_for("file");
+    let file       = metadata.location.file.as_ref().map_or(mem.symbol_for("<stdin>"), |f| string_to_list(mem, &f.clone().into_os_string().into_string().unwrap()));
+    let line_sym   = mem.symbol_for("line");
+    let line       = mem.allocate_number(metadata.location.line as i64);
+    let column_sym = mem.symbol_for("column");
+    let column     = mem.allocate_number(metadata.location.column as i64);
+    let vec        = vec![mem.allocate_cons(doc_sym, doc), mem.allocate_cons(file_sym, file), mem.allocate_cons(line_sym, line), mem.allocate_cons(column_sym, column)];
+
+    NativeResult::Value(vec_to_list(mem, &vec))
 }
