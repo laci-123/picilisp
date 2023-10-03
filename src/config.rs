fdc99aefe3ce31343ee5f793110a70937a780ec8
Gohér László <goher.laci@gmail.com>
2023-10-03 12:10:01 +0200

    simpler name for config module

--- src/config.rs
+++ src/config.rs
@@ -2,11 +2,11 @@
 pub const INITIAL_FREE_CELLS: usize = 256;
 
 /// maximum ratio of the number of free cells after garbage collection, compared to the number of used cells
-pub const MAXIMUM_FREE_RATIO: f32   = 0.75;
+pub const MAXIMUM_FREE_RATIO: f32 = 0.75;
 
 /// when removing free cells after garbage collection, keep as many that the ratio of their number and the number
 /// of used cells is at least this big
-pub const MINIMUM_FREE_RATIO: f32   = 0.1;
+pub const MINIMUM_FREE_RATIO: f32 = 0.1;
 
 /// when there are no more free cells (not even after garbage collection), allocate this many times the used cells
 pub const ALLOCATION_RATIO: f32 = 1.0;
