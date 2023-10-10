1bec309f2fafe74989542558312309dc76fb7c8d
Gohér László <goher.laci@gmail.com>
2023-10-10 17:46:45 +0200

    evaulation runs on different thread

--- src/ui/gui/mod.rs
+++ src/ui/gui/mod.rs
@@ -4,44 +4,88 @@ use crate::native::eval::eval_external;
 use crate::native::load_native_functions;
 use crate::config;
 use eframe::{App, Frame, egui, epaint, NativeOptions, run_native};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 
 
 struct Window {
-    mem: Memory, 
+    to_worker: mpsc::Sender<String>,
+    from_worker: mpsc::Receiver<Result<String, String>>,
     program_text: String,
     result_text: String,
     signal_text: Option<String>,
+    working: bool,
 }
 
 impl Window {
     fn new() -> Self {
-        let mut this = Self{ mem: Memory::new(), program_text: String::new(), result_text: String::new(), signal_text: None };
+        let (to_worker_tx,   to_worker_rx)   = mpsc::channel::<String>();
+        let (from_worker_tx, from_worker_rx) = mpsc::channel::<Result<String, String>>();
 
-        load_native_functions(&mut this.mem);
+        thread::spawn(move || {
+            let mut mem = Memory::new();
 
-        if let Err(err) = super::load_prelude(&mut this.mem) {
-            this.signal_text = Some(err);
+            load_native_functions(&mut mem);
+
+            thread::sleep(std::time::Duration::from_secs(2));
+
+            match super::load_prelude(&mut mem) {
+                Ok(_) => {
+                    from_worker_tx.send(Ok("\"loaded prelude\"".to_string())).expect("no receiver for worker thread messages");
+                },
+                Err(err) => {
+                    from_worker_tx.send(Err(err)).expect("no receiver for worker thread messages");
+                },
+            }
+
+            for input in to_worker_rx.iter() {
+                // (read-eval-print "input string...")
+                let vec        = vec![mem.symbol_for("read-eval-print"), string_to_proper_list(&mut mem, &input)];
+                let expression = vec_to_list(&mut mem, &vec);
+                match eval_external(&mut mem, expression) {
+                    Ok(x)    => {
+                        let output = list_to_string(x).expect("read-eval-print returned something that cannot be converted to a string");
+                        from_worker_tx.send(Ok(output)).expect("no receiver for worker-thread messages");
+                    },
+                    Err(err) => {
+                        from_worker_tx.send(Err(err)).expect("no receiver for worker thread messages");
+                    },
+                }
+            }
+        });
+
+        Self {
+            program_text: String::new(),
+            result_text: String::new(),
+            signal_text: None,
+            to_worker: to_worker_tx,
+            from_worker: from_worker_rx,
+            working: true,
         }
+    }
 
-        this
+    fn update(&mut self) {
+        match self.from_worker.try_recv() {
+            Ok(Ok(x))    => {
+                self.result_text = x;
+                self.signal_text = None;
+                self.working = false;
+            },
+            Ok(Err(err)) => {
+                self.signal_text = Some(err);
+                self.result_text.clear();
+                self.working = false;
+            },
+            Err(_)       => {},
+        }
     }
 
     fn eval(&mut self) {
         self.signal_text = None;
-        
-        // (read-eval-print "input string...")
-        let vec        = vec![self.mem.symbol_for("read-eval-print"), string_to_proper_list(&mut self.mem, &self.program_text)];
-        let expression = vec_to_list(&mut self.mem, &vec);
-        match eval_external(&mut self.mem, expression) {
-            Ok(x) => {
-                self.result_text = list_to_string(x).unwrap()
-            },
-            Err(x) => {
-                self.result_text = String::new();
-                self.signal_text = Some(x);
-            },
-        }
+        self.to_worker.send(self.program_text.clone()).expect("worker thread dissappeared");
+        self.working = true;
     }
 }
 
@@ -54,12 +98,22 @@ impl App for Window {
             ui.heading(config::APPLICATION_NAME);
             ui.add_space(10.0);
 
+            self.update();
+
             ui.add(egui::TextEdit::multiline(&mut self.program_text).font(egui::FontId::monospace(12.0)));
             ui.add_space(10.0);
-            if ui.button("Evaluate").clicked() {
-                self.eval();
-            }
+
+            ui.horizontal(|ui| {
+                if ui.button("Evaluate").clicked() {
+                    self.eval();
+                }
+                if self.working {
+                    ui.label("working...");
+                    ctx.request_repaint_after(Duration::from_millis(500));
+                }
+            });
             ui.add_space(10.0);
+
             ui.add(egui::TextEdit::multiline(&mut self.result_text).font(egui::FontId::monospace(12.0)));
             if let Some(x) = &self.signal_text {
                 // passing a mutable reference to an immutable str to TextEdit::multiline
