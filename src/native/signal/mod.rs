7eb04f7afd6e0c9a55d3d49870cead3138de68d9
Gohér László <goher.laci@gmail.com>
2023-12-14 14:28:04 +0100

    signal function doesn't accept nil as argument

--- src/native/signal/mod.rs
+++ src/native/signal/mod.rs
@@ -15,6 +15,14 @@ NativeFunctionMetaData{
 
 pub fn signal(mem: &mut Memory, args: &[GcRef], _env: GcRef, _recursion_depth: usize) -> Result<GcRef, GcRef> {
     validate_args!(mem, SIGNAL.name, args, (let signal: TypeLabel::Any));
+
+    if signal.is_nil() {
+        let error_details = vec![("argument-value", signal.clone()),
+                                 ("expected", mem.symbol_for("any-non-nil-type")),
+                                 ("actual", mem.symbol_for("nil-type"))];
+        let error         = make_error(mem, "wrong-argument-type", SIGNAL.name, &error_details);
+        return Err(error);
+    }
     
     Err(signal)
 }
